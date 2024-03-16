0f3399d7de653a69bd85be7c1368ea4003246123
mahadi025 <mahadikarimmunif@gmail.com>
2024-03-16 13:23:09 +0600

    moved the third party imports to shared module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,12 +5,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HttpClientModule } from '@angular/common/http';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
+import { RecipeComponent } from './recipe/recipe/recipe.component';
+import { SharedModule } from './_modules/shared.module';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule, BsDropdownModule.forRoot(), BrowserAnimationsModule],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    RegisterComponent,
+    HomeComponent,
+    RecipeListComponent,
+    RecipeComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    SharedModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
