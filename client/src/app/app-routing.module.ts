29f2d71e462b21062877f780b78be6cab41441f0
mahadi025 <mahadikarimmunif@gmail.com>
2024-03-16 13:22:01 +0600

    implemented routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
+import { RecipeComponent } from './recipe/recipe/recipe.component';
+import { RegisterComponent } from './register/register.component';
+import { authGuard } from './_guards/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'register', component: RegisterComponent },
+
+  {
+    path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [authGuard],
+    children: [
+      { path: 'recipes', component: RecipeListComponent },
+      { path: 'recipes:/id', component: RecipeComponent },
+    ],
+  },
+
+  { path: '**', component: HomeComponent, pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
