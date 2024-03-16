a20a2fac5fdeffb7b0cdfb09e1291911adec74c2
mahadi025 <mahadikarimmunif@gmail.com>
2024-03-16 13:21:07 +0600

    added name to the response while making a post request for token

--- api/user/views.py
+++ api/user/views.py
@@ -25,7 +25,7 @@ class CreateTokenView(ObtainAuthToken):
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data["user"]
         token, created = Token.objects.get_or_create(user=user)
-        return Response({"token": token.key, "email": user.email})
+        return Response({"token": token.key, "email": user.email, "name": user.name})
 
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
