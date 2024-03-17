22a618064fe8fb9a2b861777cab02ceb4ec9eff9
mahadi025 <mahadikarimmunif@gmail.com>
2024-03-17 14:40:18 +0600

    implemented recipe-details

--- client/src/app/recipe/recipe-details/recipe-details.component.ts
+++ client/src/app/recipe/recipe-details/recipe-details.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Recipe } from 'src/app/_models/recipe';
+import { RecipesService } from 'src/app/_services/recipes.service';
 
 @Component({
   selector: 'app-recipe-details',
   templateUrl: './recipe-details.component.html',
-  styleUrls: ['./recipe-details.component.css']
+  styleUrls: ['./recipe-details.component.css'],
 })
-export class RecipeDetailsComponent {
+export class RecipeDetailsComponent implements OnInit {
+  recipe: Recipe | undefined;
 
+  constructor(
+    private recipeService: RecipesService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.loadRecipe();
+  }
+
+  loadRecipe() {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (!id) return;
+    this.recipeService.getRecipe(id).subscribe({
+      next: (recipe) => (this.recipe = recipe),
+    });
+  }
 }
